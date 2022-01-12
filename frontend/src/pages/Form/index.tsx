1832f8f4a4ce3808d1b7df017f420fc2f2b42958
rodrigoaraujoti <rodrigoaraujoeti@gmail.com>
2022-01-11 23:59:44 -0300

    Pagination layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -5,7 +5,7 @@ function Form() {
     id: 1,
     image:
       "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcherrrrr",
+    title: "The Witcher",
     count: 2,
     score: 4.5,
   };
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,10 @@
+import Pagination from "components/Pagination";
+import './styles.css';
+
 function Listing() {
-  return <h1>Página de listagem</h1>;
+  return (
+    <Pagination />
+  );
 }
 
 export default Listing;
